f6a6830ca2c5ae83461d9e86695f0ebc6815fec4
龙勤思 <longqinsi@hotmail.com>
2021-09-02 15:34:54 +0800

    The Content Class#fixed

--- ssg/content.py
+++ ssg/content.py
@@ -5,12 +5,12 @@ from collections.abc import Mapping
 
 class Content(Mapping):
 
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
-        _, fm, content = Content.__regex.split(string=string, maxsplit=2)
+        _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
@@ -28,8 +28,8 @@ class Content(Mapping):
         return self.data["type"] if "type" in self.data else None
 
     @type.setter
-    def type(self, value):
-        self.data["type"] = value
+    def type(self, type):
+        self.data["type"] = type
 
     def __getitem__(self, key):
         return self.data[key]
@@ -43,6 +43,6 @@ class Content(Mapping):
     def __repr__(self) -> str:
         data = {}
         for key, value in self.data.items():
-            if key == "content":
+            if key != "content":
                 data[key] = value
         return str(data)
